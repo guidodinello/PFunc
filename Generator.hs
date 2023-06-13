480b0f19a146acb614f35ee2d35dbb48303ac50e
Guido Dinello <guido.dinello@gmail.com>
2023-06-13 00:01:47 -0300

    let expressions handling for genProgram

--- Generator.hs
+++ Generator.hs
@@ -12,9 +12,9 @@ module Generator where
 import Syntax
 
 -- se pueden agregar mas importaciones
--- en caso de ser necesario
+-- en caso de ser necesario 
 
-import Data.List
+import Data.List ( intercalate )
 
 -- CODE GENERATOR
 
@@ -22,43 +22,84 @@ import Data.List
 genProgram :: Program -> String
 genProgram (Program defs expr) = 
     "#include <stdio.h>\n" ++
-    concatMap defRepr defs ++
+    concatMap defRepr0 defs ++
     "int main() {\n" ++
-    "printf(\"%d\\n\"," ++ exprRepr expr ++ "); }\n"
+    letDefs ++
+    "printf(\"%d\\n\"," ++ bodyRepr ++ "); }\n"
+    where
+        defRepr0 = defRepr 0
+        (bodyRepr, letDefs, _) = exprRepr expr "" 0
 
 -- -- genera el código C correspondiente a las definiciones de funciones
-defRepr :: FunDef -> String
-defRepr (FunDef (name, Sig sigTypes retType) args expr) =
+defRepr :: Int -> FunDef -> String
+defRepr depth (FunDef (name, Sig sigTypes retType) args expr) =
     typeRepr retType ++ " _" ++ name ++ "(" ++ typedArgsString ++ "){\n" ++
-    "return (" ++ exprRepr expr ++ "); };\n"
-    where
+    letDefs ++
+    "return (" ++ bodyRepr ++ "); };\n"
+    where 
+        -- [TyInt, TyBool] ["x", "y"] -> "int _x, int _y"
         typedArgsString = intercalate "," $ zipWith 
-                (\sigType name -> typeRepr sigType ++ " _" ++ name)
-                sigTypes args
+            (\sigType name -> typeRepr sigType ++ " _" ++ name)
+            sigTypes args
+        (bodyRepr, letDefs, _) = exprRepr expr "" 0
 
 typeRepr :: Type -> String
 -- no hay booleanos en C, se usan enteros
 typeRepr _ = "int"
 
-exprRepr :: Expr -> String
-exprRepr (Var name) = "_" ++ name
-exprRepr (IntLit num) = show num
-exprRepr (BoolLit value)
-    | value = "1"
-    | otherwise = "0"
-exprRepr (Infix op lExpr rExpr) = "(" ++ exprRepr lExpr ++ show op ++ exprRepr rExpr ++ ")"
--- _b?(_x * _x):(_x + 2) 
-exprRepr (If condExpr thenExpr elseExpr) =  
-    exprRepr condExpr ++ "?(" ++ exprRepr thenExpr ++ "):(" ++ exprRepr elseExpr ++ ")"
-{-
-(let x :: Int = y in x)
-              + (let y :: Int = x + y in y)
-int _let0(int _x){
-return (_x); };
-int _let1(int _y){
-return (_y); };
-return ((_let0(_y) + _let1((_x + _y))));
--}
-exprRepr (Let typedVar eqExpr inExpr) = undefined
-exprRepr (App name exprs) = "_" ++ name ++ "(" ++ intercalate "," (map exprRepr exprs) ++ ")"
+opRepr :: Op -> String
+opRepr Eq = "=="
+-- definieron el show de los Op como " op " pero en los test Eq aparece como "op"
+opRepr op = show op
 
+exprRepr :: Expr -> String -> Int -> (String, String, Int)
+-- el primer elemento es la representación de la expresión
+-- el segundo es el string generado para las definiciones de las expresiones let (si las hay)
+-- el tercero es el contador de expresiones let que sirve para su numeracion
+exprRepr (Var name) defs count = ("_" ++ name, defs, count)
+exprRepr (IntLit num) defs count = (show num, defs, count)
+exprRepr (BoolLit value) defs count
+    -- C no tiene booleanos, se usan enteros
+    | value = ("1", defs, count)
+    | otherwise = ("0", defs, count)
+exprRepr (Infix op lExpr rExpr) defs count =
+    let (lExprRepr, lDefs, lCount) = exprRepr lExpr defs count
+        (rExprRepr, rDefs, rCount) = exprRepr rExpr defs lCount
+    in ("(" ++ lExprRepr ++ opRepr op ++ rExprRepr ++ ")", lDefs ++ rDefs, rCount)
+exprRepr (If condExpr thenExpr elseExpr) defs count =
+    let (condRepr, condDefs, condCount) = exprRepr condExpr defs count
+        (thenRepr, thenDefs, thenCount) = exprRepr thenExpr condDefs condCount
+        (elseRepr, elseDefs, elseCount) = exprRepr elseExpr thenDefs thenCount
+    in (condRepr ++ "?" ++ thenRepr ++ ":" ++ elseRepr, elseDefs, elseCount)
+exprRepr (Let (varName, varType) eqExpr inExpr) defs count = 
+    let (eqExprRepr, eqDefs, eqCount) = exprRepr eqExpr defs count
+        (inExprRepr, inDefs, inCount) = exprRepr inExpr defs eqCount
+
+        letId = "_let" ++ show inCount
+        letDef = typeRepr varType ++ " " ++ letId ++ "(" ++ typeRepr varType ++ " _" ++ varName ++ "){\n"
+        letBody = "return (" ++ inExprRepr ++ "); };\n"
+
+    in (letId ++ "(" ++ eqExprRepr ++ ")", eqDefs ++ letDef ++ inDefs ++ letBody, inCount + 1)
+exprRepr (App name exprs) defs count =
+    -- h(e1, e2, ...) -> _h( exprRepr e1, exprRepr e2, ...)
+    -- obs: si hay let en varias expresiones el contador debe irse acumulando dado 
+    -- que las definiciones de los let estaran en el mismo scope.
+    -- idem con las definiciones de las expresiones let
+    let (bodyReprs, letDefs, exprCount) = mapAccum exprRepr exprs defs count
+        bodyRepr = intercalate "," (map (\(r, _, _) -> r) bodyReprs)
+        exprDefs = concatMap (\(_, d, _) -> d) bodyReprs
+    
+    in ("_" ++ name ++ "(" ++ bodyRepr ++ ")", letDefs ++ exprDefs, exprCount)
+
+mapAccum :: 
+    -- funcion de mapeo
+    (Expr -> String -> Int -> (String, String, Int)) -> 
+    --  lista de expresiones, string definiciones de let, contador de let
+    [Expr] -> String -> Int -> 
+    -- lista de representaciones de expresiones, string definiciones de let, contador de let
+    ([(String, String, Int)], String, Int)
+mapAccum _ [] defs count = ([], defs, count)
+mapAccum f (x:xs) defs count =
+    let (xRepr, xDefs, xCount) = f x defs count
+        (xsReprs, xsDefs, xsCount) = mapAccum f xs xDefs xCount
+    in ((xRepr, xDefs, xCount) : xsReprs, xsDefs, xsCount)
