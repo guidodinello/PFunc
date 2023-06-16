3e8e894f26c550cc6f816f4886a58cb5994f43da
Josefina Guberna <jguberna96@gmail.com>
2023-06-15 21:14:54 -0300

    elim let

--- LetElim.hs
+++ LetElim.hs
@@ -18,7 +18,50 @@ import Syntax
 -- se utiliza en el proceso de optimizacion
 -- se puede implementar ultimo
 letElimP :: Program -> Program
-letElimP = undefined
+letElimP (Program defs main) = Program newDefs newMain
+    where newDefs = map letElimDef defs
+          newMain = letElimExpr main
 
+letElimDef :: FunDef -> FunDef
+letElimDef (FunDef t n e) = FunDef t n newExpr
+    where newExpr = letElimExpr e
+
+letElimExpr :: Expr -> Expr
+letElimExpr (Let (n,_) lit@(IntLit i) e2) = letElimExpr e2'
+    where e2' = subst n lit e2
+letElimExpr (Let (n,_) lit@(BoolLit b) e2) = letElimExpr e2'
+    where e2' = subst n lit e2
+letElimExpr (Let t e1 e2) = Let t e1' e2'
+    where   e1' = letElimExpr e1
+            e2' = letElimExpr e2
+letElimExpr (Infix op e1 e2) =  Infix op e1' e2'
+    where   e1' = letElimExpr e1
+            e2' = letElimExpr e2
+letElimExpr (If e1 e2 e3) = If e1' e2' e3'
+    where   e1' = letElimExpr e1
+            e2' = letElimExpr e2
+            e3' = letElimExpr e3
+letElimExpr (App n es) = App n es'
+    where es' = map letElimExpr es
+letElimExpr e = e
+
+
+-- La invocacion subst x e1 e2 implementa la sustitucion de las ocurrencias libres de x en e2 por e1
 subst :: Name -> Expr -> Expr -> Expr
-subst = undefined
+subst n lit e@(Var name)
+    | n == name = lit
+    | otherwise = e
+subst n lit (Infix o e1 e2) = Infix o e1' e2'
+    where   e1' = subst n lit e1
+            e2' = subst n lit e1
+subst n lit (If e1 e2 e3) = If e1' e2' e3'
+    where   e1' = subst n lit e1
+            e2' = subst n lit e1
+            e3' = subst n lit e3
+subst n lit (Let t e1 e2) = Let t e1' e2'
+    where   e1' = subst n lit e1
+            e2' = subst n lit e1
+subst n lit (App name es) = App name es'
+    where es' = map (subst n lit) es
+subst _ _ e = e
+
